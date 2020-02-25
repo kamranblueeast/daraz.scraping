f362f11643f451f40cc5ee82aa3b9a92a19befbe
kamran <kamran.rafi@blueeast.com>
2020-02-25 18:54:47 +0500

    env config added

--- snip.js
+++ snip.js
@@ -1,9 +1,19 @@
 const puppeteer = require('puppeteer');
+const express = require('express');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const slug = require('slug');
 const fs = require('fs')
+const app = express();
 
-async function getProd() {
+const port = process.env.PORT || 80;
+
+const expressServer = app.listen(port, async () => {
+    const server = expressServer.address();
+    const { address, port } = server;
+
+    console.log(`Bot listening at => ${address}:${port}`);
+});
+app.get("/scraper", async (req, res) => {
     const url = 'http://www.sinoped.net/productgrouplist-806538536-3/Clean_room.html?spm=a2700.icbuShop.41413.44.7f9136cbUbNWLw&filter=null'
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
@@ -33,4 +43,4 @@ async function getProd() {
     })
     console.log('thumbs', thumbs)
 }
-getProd();
+);
